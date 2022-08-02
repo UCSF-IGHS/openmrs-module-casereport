347450ef6c6f774e41eed9e2c3d57edc76573d52
Eudson Bambo <eudson.bambo@gmail.com>
2022-08-02 17:38:51 +0200

    Bug fix: added check for concept mappings size on activator

--- api/src/main/java/org/openmrs/module/casereport/CaseReportActivator.java
+++ api/src/main/java/org/openmrs/module/casereport/CaseReportActivator.java
@@ -3,16 +3,11 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
  * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- * 
+ * <p>
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
 package org.openmrs.module.casereport;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
@@ -29,162 +24,170 @@ import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.openmrs.scheduler.SchedulerService;
 import org.openmrs.scheduler.TaskDefinition;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
  */
 public class CaseReportActivator extends BaseModuleActivator {
-	
-	protected Log log = LogFactory.getLog(getClass());
-	
-	/**
-	 * @see ModuleActivator#willRefreshContext()
-	 */
-	public void willRefreshContext() {
-		log.info("Refreshing Case Report Module");
-	}
-	
-	/**
-	 * <strong>Should</strong> fail for a query with no name
-	 * <strong>Should</strong> fail for a query with no sql
-	 * <strong>Should</strong> fail for a query with no concept
-	 * <strong>Should</strong> fail for a query with a none ciel concept
-	 * <strong>Should</strong> fail for a query with an invalid concept mapping
-	 * <strong>Should</strong> fail for a query with a none existent concept
-	 * <strong>Should</strong> ignore a cohort query with a duplicate name
-	 * <strong>Should</strong> save a cohort queries with a name that matches a retired duplicate
-	 * <strong>Should</strong> load queries and register them with the reporting module
-	 * <strong>Should</strong> add the case report tasks if they do not exist
-	 * @see ModuleActivator#contextRefreshed()
-	 */
-	public void contextRefreshed() {
-		loadQueries();
-		log.info("Case Report Module refreshed");
-	}
-	
-	private void loadQueries() {
-		log.info("Loading queries...");
-		
-		List<SqlCohortQueryLoader> loaders = new ArrayList<SqlCohortQueryLoader>();
-		loaders.addAll(Context.getRegisteredComponents(SqlCohortQueryLoader.class));
-		if (loaders.isEmpty()) {
-			loaders.add(new DefaultSqlCohortQueryLoader());
-		}
-		
-		List<SqlCohortQuery> cohortQueries = new ArrayList<SqlCohortQuery>();
-		for (SqlCohortQueryLoader loader : loaders) {
-			try {
-				cohortQueries.addAll(loader.load());
-			}
-			catch (IOException e) {
-				throw new APIException("Failed to load some cohort queries by SqlQueryLoader: " + loader, e);
-			}
-		}
-		
-		for (SqlCohortQuery cohortQuery : cohortQueries) {
-			if (StringUtils.isBlank(cohortQuery.getName())) {
-				throw new APIException("Failed to load cohort query because of missing name field");
-			}
-			
-			List<SqlCohortDefinition> matches = DefinitionContext.getDefinitionService(SqlCohortDefinition.class)
-			        .getDefinitions(cohortQuery.getName(), true);
-			List<SqlCohortDefinition> nonRetiredQueries = new ArrayList<SqlCohortDefinition>(matches.size());
-			for (SqlCohortDefinition match : matches) {
-				if (!match.isRetired()) {
-					nonRetiredQueries.add(match);
-				}
-			}
-			if (nonRetiredQueries.size() > 0) {
-				continue;
-			}
-			
-			if (StringUtils.isBlank(cohortQuery.getSql())) {
-				throw new APIException("Failed to load cohort query because of missing sql field");
-			} else if (StringUtils.isBlank(cohortQuery.getConcept())) {
-				throw new APIException("Failed to load cohort query because of missing concept field");
-			}
-			
-			String conceptStr = cohortQuery.getConcept();
-			if (conceptStr.startsWith(CaseReportConstants.CIEL_MAPPING_PREFIX)) {
-				CaseReportUtil.getConceptByMappingString(conceptStr, true);
-			} else {
-				throw new APIException("Only CIEL concept mappings are currently allowed");
-			}
 
-			CohortDefinition definition = new SqlCohortDefinition(cohortQuery.getSql());
-			definition.setName(cohortQuery.getName());
-			definition.setDescription(cohortQuery.getDescription());
-			if (cohortQuery.getSql().indexOf(":" + CaseReportConstants.LAST_EXECUTION_TIME) > -1) {
-				String label = Context.getMessageSourceService().getMessage("casereport.lastExecutionTime");
-				definition.addParameter(new Parameter(CaseReportConstants.LAST_EXECUTION_TIME, label, Date.class));
-			}
-			String obsConcept = "";
-			String obsvalueCoded = "";
-			if (cohortQuery.getConceptMappings() != null) {
-				obsConcept = cohortQuery.getConceptMappings().get(0);
-				obsvalueCoded = cohortQuery.getConceptMappings().get(1);
-				for (String mapping : cohortQuery.getConceptMappings()) {
-					definition.addParameter(new Parameter(mapping, mapping.replaceFirst(
-					    CaseReportConstants.CONCEPT_MAPPING_SEPARATOR, ":"), Concept.class));
+    protected Log log = LogFactory.getLog(getClass());
+
+    /**
+     * @see ModuleActivator#willRefreshContext()
+     */
+    public void willRefreshContext() {
+        log.info("Refreshing Case Report Module");
+    }
+
+    /**
+     * <strong>Should</strong> fail for a query with no name
+     * <strong>Should</strong> fail for a query with no sql
+     * <strong>Should</strong> fail for a query with no concept
+     * <strong>Should</strong> fail for a query with a none ciel concept
+     * <strong>Should</strong> fail for a query with an invalid concept mapping
+     * <strong>Should</strong> fail for a query with a none existent concept
+     * <strong>Should</strong> ignore a cohort query with a duplicate name
+     * <strong>Should</strong> save a cohort queries with a name that matches a retired duplicate
+     * <strong>Should</strong> load queries and register them with the reporting module
+     * <strong>Should</strong> add the case report tasks if they do not exist
+     * @see ModuleActivator#contextRefreshed()
+     */
+    public void contextRefreshed() {
+        loadQueries();
+        log.info("Case Report Module refreshed");
+    }
+
+    private void loadQueries() {
+        log.info("Loading queries...");
+
+        List<SqlCohortQueryLoader> loaders = new ArrayList<SqlCohortQueryLoader>();
+        loaders.addAll(Context.getRegisteredComponents(SqlCohortQueryLoader.class));
+        if (loaders.isEmpty()) {
+            loaders.add(new DefaultSqlCohortQueryLoader());
+        }
+
+        List<SqlCohortQuery> cohortQueries = new ArrayList<SqlCohortQuery>();
+        for (SqlCohortQueryLoader loader : loaders) {
+            try {
+                cohortQueries.addAll(loader.load());
+            } catch (IOException e) {
+                throw new APIException("Failed to load some cohort queries by SqlQueryLoader: " + loader, e);
+            }
+        }
+
+        for (SqlCohortQuery cohortQuery : cohortQueries) {
+            if (StringUtils.isBlank(cohortQuery.getName())) {
+                throw new APIException("Failed to load cohort query because of missing name field");
+            }
+
+            List<SqlCohortDefinition> matches = DefinitionContext.getDefinitionService(SqlCohortDefinition.class)
+                    .getDefinitions(cohortQuery.getName(), true);
+            List<SqlCohortDefinition> nonRetiredQueries = new ArrayList<SqlCohortDefinition>(matches.size());
+            for (SqlCohortDefinition match : matches) {
+                if (!match.isRetired()) {
+                    nonRetiredQueries.add(match);
+                }
+            }
+            if (nonRetiredQueries.size() > 0) {
+                continue;
+            }
+
+            if (StringUtils.isBlank(cohortQuery.getSql())) {
+                throw new APIException("Failed to load cohort query because of missing sql field");
+            } else if (StringUtils.isBlank(cohortQuery.getConcept())) {
+                throw new APIException("Failed to load cohort query because of missing concept field");
+            }
+
+            String conceptStr = cohortQuery.getConcept();
+            if (conceptStr.startsWith(CaseReportConstants.CIEL_MAPPING_PREFIX)) {
+                CaseReportUtil.getConceptByMappingString(conceptStr, true);
+            } else {
+                throw new APIException("Only CIEL concept mappings are currently allowed");
+            }
+
+            CohortDefinition definition = new SqlCohortDefinition(cohortQuery.getSql());
+            definition.setName(cohortQuery.getName());
+            definition.setDescription(cohortQuery.getDescription());
+            if (cohortQuery.getSql().indexOf(":" + CaseReportConstants.LAST_EXECUTION_TIME) > -1) {
+                String label = Context.getMessageSourceService().getMessage("casereport.lastExecutionTime");
+                definition.addParameter(new Parameter(CaseReportConstants.LAST_EXECUTION_TIME, label, Date.class));
+            }
+            String obsConcept = "";
+            String obsvalueCoded = "";
+            if (cohortQuery.getConceptMappings() != null) {
+                obsConcept = cohortQuery.getConceptMappings().get(0);
+
+                if (cohortQuery.getConceptMappings().size() > 1) {
+					obsvalueCoded = cohortQuery.getConceptMappings().get(1);
 				}
-			}
-			DefinitionContext.saveDefinition(definition);
-			addSchedulerTaskIfNecessary(cohortQuery.getName(), conceptStr, cohortQuery.getRepeatInterval(),
-			    cohortQuery.getAutoSubmit(), obsConcept, obsvalueCoded);
-		}
-	}
-	
-	private void addSchedulerTaskIfNecessary(String name, String concept, Long repeatInterval, Boolean autoSubmit,
-			String obsConcept, String obsvalueCoded) {
-		
-		SchedulerService ss = Context.getSchedulerService();
-		String className = CaseReportTask.class.getName();
-		String description = Context.getMessageSourceService().getMessage("casereport.description.schedulerTaskFor",
-		    new Object[] { name }, null, null);
-		TaskDefinition td = ss.getTaskByName(name);
-		if (td == null || !className.equals(className)) {
-			td = new TaskDefinition(null, name, description, className);
-			td.setStartOnStartup(false);
-			td.setProperty(CaseReportConstants.TRIGGER_NAME_TASK_PROPERTY, name);
-			td.setProperty(CaseReportConstants.CONCEPT_TASK_PROPERTY, concept);
-			td.setProperty(CaseReportConstants.OBS_CONCEPT, obsConcept);
-			td.setProperty(CaseReportConstants.OBS_VALUE_CODED, obsvalueCoded);
-			td.setRepeatInterval(repeatInterval);
-			if (Boolean.TRUE.equals(autoSubmit)) {
-				td.setProperty(CaseReportConstants.AUTO_SUBMIT_TASK_PROPERTY, "true");
-			}
-			if (td.getRepeatInterval() == null) {
-				td.setRepeatInterval(0L);
-			}
-			ss.saveTaskDefinition(td);
-		}
-	}
-	
-	/**
-	 * @see ModuleActivator#willStart()
-	 */
-	public void willStart() {
-		log.info("Starting Case Report Module");
-	}
-	
-	/**
-	 * @see ModuleActivator#started()
-	 */
-	public void started() {
-		log.info("Case Report Module started");
-	}
-	
-	/**
-	 * @see ModuleActivator#willStop()
-	 */
-	public void willStop() {
-		log.info("Stopping Case Report Module");
-	}
-	
-	/**
-	 * @see ModuleActivator#stopped()
-	 */
-	public void stopped() {
-		log.info("Case Report Module stopped");
-	}
-	
+
+                for (String mapping : cohortQuery.getConceptMappings()) {
+                    definition.addParameter(new Parameter(mapping, mapping.replaceFirst(
+                            CaseReportConstants.CONCEPT_MAPPING_SEPARATOR, ":"), Concept.class));
+                }
+            }
+            DefinitionContext.saveDefinition(definition);
+            addSchedulerTaskIfNecessary(cohortQuery.getName(), conceptStr, cohortQuery.getRepeatInterval(),
+                    cohortQuery.getAutoSubmit(), obsConcept, obsvalueCoded);
+        }
+    }
+
+    private void addSchedulerTaskIfNecessary(String name, String concept, Long repeatInterval, Boolean autoSubmit,
+                                             String obsConcept, String obsvalueCoded) {
+
+        SchedulerService ss = Context.getSchedulerService();
+        String className = CaseReportTask.class.getName();
+        String description = Context.getMessageSourceService().getMessage("casereport.description.schedulerTaskFor",
+                new Object[]{name}, null, null);
+        TaskDefinition td = ss.getTaskByName(name);
+        if (td == null || !className.equals(className)) {
+            td = new TaskDefinition(null, name, description, className);
+            td.setStartOnStartup(false);
+            td.setProperty(CaseReportConstants.TRIGGER_NAME_TASK_PROPERTY, name);
+            td.setProperty(CaseReportConstants.CONCEPT_TASK_PROPERTY, concept);
+            td.setProperty(CaseReportConstants.OBS_CONCEPT, obsConcept);
+            td.setProperty(CaseReportConstants.OBS_VALUE_CODED, obsvalueCoded);
+            td.setRepeatInterval(repeatInterval);
+            if (Boolean.TRUE.equals(autoSubmit)) {
+                td.setProperty(CaseReportConstants.AUTO_SUBMIT_TASK_PROPERTY, "true");
+            }
+            if (td.getRepeatInterval() == null) {
+                td.setRepeatInterval(0L);
+            }
+            ss.saveTaskDefinition(td);
+        }
+    }
+
+    /**
+     * @see ModuleActivator#willStart()
+     */
+    public void willStart() {
+        log.info("Starting Case Report Module");
+    }
+
+    /**
+     * @see ModuleActivator#started()
+     */
+    public void started() {
+        log.info("Case Report Module started");
+    }
+
+    /**
+     * @see ModuleActivator#willStop()
+     */
+    public void willStop() {
+        log.info("Stopping Case Report Module");
+    }
+
+    /**
+     * @see ModuleActivator#stopped()
+     */
+    public void stopped() {
+        log.info("Case Report Module stopped");
+    }
+
 }
